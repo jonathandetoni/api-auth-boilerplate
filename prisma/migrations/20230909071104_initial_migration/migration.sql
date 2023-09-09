f3797e33dc7116270896d99bb8f5153b3dc49588
Jonathan de Toni <jonathan@tonidev.com.br>
2023-09-09 04:11:33 -0300

    feat: remocao de models nao utilizadas.

--- prisma/migrations/20230909071104_initial_migration/migration.sql
+++ prisma/migrations/20230909071104_initial_migration/migration.sql
@@ -71,49 +71,6 @@ CREATE TABLE "contacts" (
     CONSTRAINT "contacts_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "budgets-sent" (
-    "id" UUID NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "description" VARCHAR(255) NOT NULL,
-    "status" "StatusBudgets" NOT NULL DEFAULT 'OPEN',
-    "value" VARCHAR(255) NOT NULL,
-    "professionalId" UUID NOT NULL,
-
-    CONSTRAINT "budgets-sent_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "budgets" (
-    "id" UUID NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "name" VARCHAR(255) NOT NULL,
-    "description" VARCHAR(255) NOT NULL,
-    "status" "StatusDemands" NOT NULL DEFAULT 'NEW',
-    "category" VARCHAR(255) NOT NULL,
-    "typeService" VARCHAR(255) NOT NULL,
-    "addressId" UUID NOT NULL,
-    "ownerId" UUID NOT NULL,
-
-    CONSTRAINT "budgets_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "comments" (
-    "id" UUID NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
-    "comment" TEXT NOT NULL,
-    "visible" BOOLEAN NOT NULL DEFAULT true,
-    "parentCommentId" UUID,
-    "demandId" UUID NOT NULL,
-    "ownerCommentId" UUID NOT NULL,
-
-    CONSTRAINT "comments_pkey" PRIMARY KEY ("id")
-);
-
 -- CreateIndex
 CREATE UNIQUE INDEX "tenants_id_key" ON "tenants"("id");
 
@@ -141,21 +98,6 @@ CREATE UNIQUE INDEX "adresses_id_key" ON "adresses"("id");
 -- CreateIndex
 CREATE UNIQUE INDEX "contacts_id_key" ON "contacts"("id");
 
--- CreateIndex
-CREATE UNIQUE INDEX "budgets-sent_id_key" ON "budgets-sent"("id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "budgets_id_key" ON "budgets"("id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "budgets_name_key" ON "budgets"("name");
-
--- CreateIndex
-CREATE UNIQUE INDEX "comments_id_key" ON "comments"("id");
-
--- CreateIndex
-CREATE UNIQUE INDEX "comments_parentCommentId_key" ON "comments"("parentCommentId");
-
 -- AddForeignKey
 ALTER TABLE "users" ADD CONSTRAINT "users_tenantId_fkey" FOREIGN KEY ("tenantId") REFERENCES "tenants"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -167,21 +109,3 @@ ALTER TABLE "adresses" ADD CONSTRAINT "adresses_dataBasicUsersId_fkey" FOREIGN K
 
 -- AddForeignKey
 ALTER TABLE "contacts" ADD CONSTRAINT "contacts_dataBasicUsersId_fkey" FOREIGN KEY ("dataBasicUsersId") REFERENCES "data-basic-users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "budgets-sent" ADD CONSTRAINT "budgets-sent_professionalId_fkey" FOREIGN KEY ("professionalId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "budgets" ADD CONSTRAINT "budgets_addressId_fkey" FOREIGN KEY ("addressId") REFERENCES "adresses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "budgets" ADD CONSTRAINT "budgets_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "comments" ADD CONSTRAINT "comments_parentCommentId_fkey" FOREIGN KEY ("parentCommentId") REFERENCES "comments"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "comments" ADD CONSTRAINT "comments_demandId_fkey" FOREIGN KEY ("demandId") REFERENCES "budgets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "comments" ADD CONSTRAINT "comments_ownerCommentId_fkey" FOREIGN KEY ("ownerCommentId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
